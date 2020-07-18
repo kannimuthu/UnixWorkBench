ab192ab8a768bd5b0cdcc0bee2d07e855056e337
KANNIMUTHU SUBRAMANIAN <47733888+kannimuthu@users.noreply.github.com>
2020-07-19 01:39:30 +0530

    Add files via upload

--- game.sh
+++ game.sh
@@ -1,20 +1,20 @@
-function guess(){
-    true_ans=$(ls -l |grep "^-"|wc -l)
+function guessgame(){
+    correct_ans=$(ls -l |grep "^-"|wc -l)
     while true;
     do
         echo "Enter your number guessed"
-        read  number
-        if [ $number -lt $true_ans ]
+        read  n
+        if [ $n -lt $correct_ans ]
         then
             echo "your guess is Less then the actual number"
-        elif [ $number -gt $true_ans ]
+        elif [ $n -gt $correct_ans ]
         then
             echo "your guess is Greater then the actual number"
         else
-            echo " congratulation,you are correct"
+            echo " Great,you are correct"
         break;
         fi
     done
 }
 echo "guess the files number in the current directory!"
-guess
+guessgame
